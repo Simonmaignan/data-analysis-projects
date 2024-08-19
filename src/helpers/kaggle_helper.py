d48f190d61b326c0f90e112a0d759db84d52f2f0
Simon Maignan <simonmaignan88@gmail.com>
2024-08-19 17:19:19 +0200

    Src code structure with kaggle helper file Kaggle helper function used in e commerce project

---
+++ src/helpers/kaggle_helper.py
@@ -0,0 +1,67 @@
+import kaggle
+from typing import Dict, Optional
+import os
+from pathlib import Path
+
+kaggle.api.authenticate()
+
+
+dataset_folder = "dataset"
+
+
+def download_dataset_files(
+    dataset_author: str,
+    dataset_name: str,
+    dataset_folder: Optional[str] = "dataset",
+    force: Optional[bool] = False,
+) -> None:
+    """
+    Download the files associated with a given Kaggle data set
+
+    First check if the files associated with the Kaggle dataset are present locally and up to date
+    Download the files using the Kaggle API if the local files are not up to date or if force flag is True
+
+    Files are downloaded and unzipped inside the given dataset folder
+
+    Args:
+        dataset_author: the data set author
+        dataset_name: the data set name
+        dataset_folder: the folder where to save the files
+        force: flag to force the download even if the files are up to date locally
+    """
+    dataset_full_name = f"{dataset_author}/{dataset_name}"
+    os.makedirs(dataset_folder, exist_ok=True)
+
+    print(f"Listing local csv files in ./{dataset_folder}.")
+    local_files_dict: Dict[str, Path] = {}
+    for path in Path(dataset_folder).iterdir():
+        if path.is_file() and path.suffix == ".csv":
+            print(
+                f"File {path.name} with size {path.stat().st_size} found in ./{dataset_folder}"
+            )
+            local_files_dict[path.name] = path
+
+    download_files = False
+    print(f"Listing files associated with Kaggle dataset {dataset_full_name}.")
+    for file in kaggle.api.datasets_list_files(dataset_author, dataset_name)[
+        "datasetFiles"
+    ]:
+        file_name: str = file["name"]
+        file_size: int = file["totalBytes"]
+        print(
+            f"File {file_name} with size {file_size} retrieved from Kaggle API."
+        )
+        if (
+            file_name not in local_files_dict
+            or file_size != local_files_dict[file_name].stat().st_size
+        ):
+            print(
+                f"File {file_name} non existing locally or not having the same size."
+            )
+            download_files = True
+            break
+
+    if download_files or force:
+        kaggle.api.dataset_download_files(
+            dataset_name, path=dataset_folder, unzip=True, quiet=False
+        )
